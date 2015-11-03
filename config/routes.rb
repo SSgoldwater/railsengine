457570d3ee08e79504f24012d300d8a4a29318cb
Stanley <stanleyjsdz@gmail.com>
2015-11-03 14:14:08 -0700

    create route for find customer#find

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      resources :customers, except: [:new, :edit]
+    namespace :v1, defaults: { format: :json } do
+      resources :customers, except: [:new, :edit] do
+	collection do
+	  get "find"
+	end
+      end
+
       resources :invoices, except: [:new, :edit]
       resources :invoice_items, except: [:new, :edit]
       resources :items, except: [:new, :edit]
