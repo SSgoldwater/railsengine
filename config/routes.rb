d26ca870951417dc2bf13abe91a1041130a03175
Stanley <stanleyjsdz@gmail.com>
2015-11-02 19:33:41 -0700

    add routes for api/v1/invoiceitems

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :customers, except: [:new, :edit]
       resources :invoices, except: [:new, :edit]
+      resources :invoice_items, except: [:new, :edit]
     end
   end
 end
