cd4c1982d0bc73d254b80bbfb107e2c15e715b01
Stanley <stanleyjsdz@gmail.com>
2015-11-03 15:04:46 -0700

    create find_all method for items controller
    that returns an array of all table items that match any given attribute

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -12,6 +12,10 @@ class Api::V1::ItemsController < ApplicationController
   def find
     respond_with Item.find_by(item_params)
   end
+
+  def find_all
+    respond_with Item.where(item_params)
+  end
   
   private
 
