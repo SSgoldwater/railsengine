780fe3eb9d4e13b229c4c82fd00344343facde86
Stanley <stanleyjsdz@gmail.com>
2015-11-02 20:35:06 -0700

    add index methods to remaining controllers
    add an index method in each controller that responds with an array of that category
    
    and add respond_to: :json, :xml for responders gem

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::InvoiceItemsController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with InvoiceItem.all
+  end
 end
--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::TransactionsController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Transaction.all
+  end
+  
 end
