b53eb48f83f1e3b8da3f6909aa0a2b114fbc0e4f
Stanley <stanleyjsdz@gmail.com>
2015-11-02 18:43:19 -0700

    schema update, old migrations deleted in favor or rails g model migrations

--- db/migrate/20151103002742_create_merchants.rb
+++
@@ -1,9 +0,0 @@
-class CreateMerchants < ActiveRecord::Migration
-  def change
-    create_table :merchants do |t|
-      t.string :name
-      t.string :created_at
-      t.string :updated_at
-    end
-  end
-end
