15943572132f2793960ba0b5887335f13ea19657
Stanley <stanleyjsdz@gmail.com>
2015-11-03 14:38:06 -0700

    create invoices controller methods for find and invoice_params

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -9,4 +9,14 @@ class Api::V1::InvoicesController < ApplicationController
     respond_with Invoice.find(params[:id])
   end
 
+  def find
+    respond_with Invoice.find_by(invoice_params)
+  end
+  
+  private
+
+  def invoice_params
+    params.permit(:id, :customer_id, :merchant_id, :status)
+  end
+
 end
