ee04c58afb706a6f8c248c775c14d4f4b609d0be
Stanley <stanleyjsdz@gmail.com>
2015-11-03 14:39:16 -0700

    create transactions controller methods for find and transaction_params

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -9,4 +9,14 @@ class Api::V1::TransactionsController < ApplicationController
     respond_with Transaction.find(params[:id])
   end
   
+  def find
+    respond_with Transaction.find_by(transaction_params)
+  end
+  
+  private
+
+  def transaction_params
+    params.permit(:id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result)
+  end
+
 end
