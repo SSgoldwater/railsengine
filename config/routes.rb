768b0de500f1f32b0490a08ddad1396ee613261b
Stanley <stanleyjsdz@gmail.com>
2015-11-03 14:19:37 -0700

    create transactions#find route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,44 @@
 Rails.application.routes.draw do
+  
   namespace :api do
     namespace :v1, defaults: { format: :json } do
+
       resources :customers, except: [:new, :edit] do
 	collection do
 	  get "find"
 	end
       end
 
-      resources :invoices, except: [:new, :edit]
-      resources :invoice_items, except: [:new, :edit]
-      resources :items, except: [:new, :edit]
-      resources :merchants, except: [:new, :edit]
-      resources :transactions, except: [:new, :edit]
+      resources :invoices, except: [:new, :edit] do
+	collection do
+	  get "find"
+	end
+      end
+        
+      resources :invoice_items, except: [:new, :edit] do
+	collection do
+	  get "find"
+	end
+      end
+
+      resources :items, except: [:new, :edit] do
+	collection do
+	  get "find" 
+	end
+      end
+
+      resources :merchants, except: [:new, :edit] do
+	collection do
+	  get "find" 
+	end
+      end
+
+      resources :transactions, except: [:new, :edit] do
+        collection do
+	  get "find" 
+	end
+      end
+
     end
   end
 end
