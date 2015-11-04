30fad4592a7bb00a39085094f26580478a76078d
Stanley <stanleyjsdz@gmail.com>
2015-11-04 07:48:02 -0700

    add controller methods for relationships

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -20,11 +20,31 @@ class Api::V1::InvoicesController < ApplicationController
   def random
     respond_with Invoice.random
   end
+
+  def transactions
+    respond_with Transaction.where(invoice_params)
+  end
+
+  def invoice_items
+    respond_with InvoiceItem.where(invoice_params)
+  end
+
+  def items
+    respond_with Item.where(invoice_params)
+  end
+  
+  def customer
+    respond_with Customer.joins(:invoices).find_by(invoices: { id: params["invoice_id"]} )
+  end
+
+  def merchant
+    respond_with Merchant.joins(:invoices).find_by(invoices: { id: params["invoice_id"] })
+  end
   
   private
 
   def invoice_params
-    params.permit(:id, :customer_id, :merchant_id, :status)
+    params.permit(:id, :invoice_id, :customer_id, :merchant_id, :status)
   end
 
 end
